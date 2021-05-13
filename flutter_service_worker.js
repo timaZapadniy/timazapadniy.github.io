89f137b6bb97324401ee114ffc0825b8c9fc9e52
timaZapadniy <zapadniysoft@gmail.com>
2021-05-13 09:28:53 +0400

    11

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "9bdb2e06982814791e6548871bdc09d2",
 "/": "9bdb2e06982814791e6548871bdc09d2",
-"main.dart.js": "21d54a69d30dbad2d79188d9a51716bb",
+"main.dart.js": "e5f7815386af6ed61e962923546a48df",
 "manifest.json": "213089d9d025318c4f0f32b9bb044bda",
 "version.json": "35f8eeffc30964bc5c98f6c3faa5ec62"
 };
