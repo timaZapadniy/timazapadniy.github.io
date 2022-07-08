87f7cd45d9f6d19adaf83e9715920f26d04d76f8
timaZapadniy <zapadniysoft@gmail.com>
2022-07-08 11:52:19 +0400

    ячс

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0cdeb2ae40e14be5bb7f4733e55ab552",
-"/": "0cdeb2ae40e14be5bb7f4733e55ab552",
-"main.dart.js": "cbc093b33d12c1a60c7b915856b2b512",
+"index.html": "5b5af7bfdb3d21ffd82bc2aacabb30c8",
+"/": "5b5af7bfdb3d21ffd82bc2aacabb30c8",
+"main.dart.js": "325ee7cb08f3d1752abed12d81207c41",
 "manifest.json": "8aadfa7ba875b097ada3d66a8f60d982",
 "version.json": "add1cca23be73ce7112de8b3e96f0928"
 };
