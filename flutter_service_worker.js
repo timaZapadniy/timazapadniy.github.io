22578c16a657ee517eb1057097255401c747c5d8
Timur Khuzyagalleev <80256451+timaZapadniy@users.noreply.github.com>
2022-09-02 11:40:30 +0400

    sdv

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cafb1f789aaca7d23d956351b40e9417",
-"/": "cafb1f789aaca7d23d956351b40e9417",
-"main.dart.js": "26d7e2afe7c70443e86d3b44426b91b0",
+"index.html": "993ef4e2dee070473912de4ae782a687",
+"/": "993ef4e2dee070473912de4ae782a687",
+"main.dart.js": "bea90dbb50eb3ad54ff37c3a627751dd",
 "manifest.json": "8aadfa7ba875b097ada3d66a8f60d982",
 "version.json": "641dff0283759a192bd1d5044a391ff7"
 };
